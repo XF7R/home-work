51f923b5872b7bdc7df9f3cc233f7622317b4043
battal <almtyrybtal1@gmail.com>
2023-08-15 22:40:25 +0300

    day5

---
+++ day5/index.js
@@ -0,0 +1,25 @@
+// home work 1
+
+let Name = prompt("الإسم :")
+let Age = prompt("العمر :")
+
+if (Age > 18){
+    alert("مرحباً بك ! " + Name)
+}
+else {
+    alert("نعتذر لعدم قبولك " + Name)
+}
+
+// home work 2
+
+let Name_ = prompt (" ادخال الإسم " )
+if (Name_ == "أحمد") {
+    alert("ال‘سمان غير متطابقان")
+}
+else {
+    alert("الإسمان متطابقان")
+}
+
+// صل على النبي 
+// استغفر الله
+// لا حول ولا قوة إلا بالله 
