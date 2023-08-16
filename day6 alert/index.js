61bcf0eb4ba0f3f9ff27252f21f83664a9a11377
battal <almtyrybtal1@gmail.com>
2023-08-17 00:23:38 +0300

    day6

---
+++ day6 alert/index.js
@@ -0,0 +1,23 @@
+//  home work 1 
+
+for (let i =1; i <= 30; i++) {
+    if (i == 11) {
+        break;
+    }
+    alert(i);
+}
+//  home work 2
+for (let i = 1; i <= 20; i++) {
+    if (i == 10) {
+        continue;
+    }
+    alert(i);
+
+}
+
+// home work 3
+let i = prompt("0 اكتب")
+do {
+    alert(i);
+    i++
+} while(i <= 5)
---
+++ day6/index.js
@@ -0,0 +1,61 @@
+////////////////////////
+
+// let Name = prompt("ًالعمر")
+// let Age = prompt(" العمر ")
+// if (Age_ > 18){
+//     alert(" انت غير مؤهل للإجازة" )
+// }
+// else {
+//     alert("   انت مؤهل" )
+// }
+// ///////////////////////
+
+// let Name_ = prompt ("  هل تعمل كموضف؟  " )
+// if (Name_ == "نعم") {
+//     alert("أنت غير مؤهل ")
+// }
+// else {
+//     alert("انت مؤهل  ")
+// }
+
+// const ff = ["Apple", "banana", "orange" , "Mango" ];
+// for(let i = 0; i < ff.length; i++) {
+//     console.log(ff [i]);
+// }
+
+// let i = 1;
+// while (i <= 11 ) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 10;
+// do {
+//     console.log(i);
+//     i++
+// } while(i <= 5);
+
+
+let i = ("*****");
+let a = ("****");
+let s = ("***");
+let d = ("**");
+let z = ("*");
+do {
+    console.log(i);
+} while (a <= 5)
+do {
+    console.log(a);
+} while (a <= 5)
+do {
+    console.log(s);
+} while (a <= 5)
+do {
+    console.log(d);
+} while (a <= 5)
+
+do {
+    console.log(z);
+    i++
+} while ( i <= 5);
+
